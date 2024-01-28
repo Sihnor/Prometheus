7c0bb36ef9a082cd66d7ec47d6d6353016d065bc
Marcel <54372177+Sihnor@users.noreply.github.com>
2024-01-28 15:55:41 +0100

    Add glm and Vulkan with a test window

--- Prometheus/src/Prometheus/Core/EntryPoint.h
+++ Prometheus/src/Prometheus/Core/EntryPoint.h
@@ -4,6 +4,14 @@
 #pragma once
 #include <iostream>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 
 #ifdef PS_PLATFORM_WINDOWS
 
@@ -13,9 +21,27 @@ int main(int argc, char *argv[]) {
     (void)argc;
     argv[0] = nullptr;
 
-    auto app = Prometheus::CreateApplication();
-    app->Run();
-    delete app;
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extension supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
 
     return 0;
 }
